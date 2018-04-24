3af489093bada35fea9a79d737f4b35f6dbecf61
Auwal MS <auwalforall@gmail.com>
2018-04-24 08:32:25 +0100

    scaffold project structure

---
+++ src/components/list-container.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      newsList: null,
+      loading: true
+    };
+  },
+  created() {
+    this.getCurrentHeadlines();
+  },
+  methods: {
+    getCurrentHeadlines() {
+      axios
+        .get("url")
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
